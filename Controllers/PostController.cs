61075a5424e1896f509b833ff68dc6197da8c91e
Benyamin Limanto <blbenyamin9@gmail.com>
2022-03-22 23:06:41 +0700

    Update and Delete example

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -11,6 +11,7 @@ namespace MvcNet.Controllers {
             _db = db;
         }
 
+        [HttpGet("/Post")]
         public ActionResult Index() {
             var data = _db.posts.ToList();
             return View("/Views/Blog/Index.cshtml",data);
@@ -27,6 +28,33 @@ namespace MvcNet.Controllers {
             await _db.SaveChangesAsync();
             TempData["message"] = "Success Add Post";
             return Redirect("~/Post/new");
+        }
+
+        [HttpGet("/Post/{id}/delete")]
+        public async Task<ActionResult> DeletePost(int id) {
+            // @see  https://stackoverflow.com/a/56537176/4906348
+            var post = new PostModel {
+                id = id
+            };
+            _db.posts.Remove(post);
+            await _db.SaveChangesAsync();
+            TempData["message"] = "Success Delete Post";
+            return Redirect("~/Post");
+        }
+
+        [HttpGet("/Post/{id}")]
+        public ActionResult UpdatePost(int id) {
+            var data = _db.posts.Where(i => i.id == id).Single();
+
+            return View("/Views/Post/EditPost.cshtml",data);
+        }
+
+        [HttpGet("/Post/{id}")]
+        public async Task<ActionResult> UpdatePostData(int id, PostModel data) {
+            _db.posts.Update(data);
+            await _db.SaveChangesAsync();
+            
+            return View("/Views/Post/EditPost.cshtml",data);
         }
     }
 }
